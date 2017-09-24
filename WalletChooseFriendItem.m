30e370a365af2696f717df052fec06c6db230c9e
vanyamelikov <vanyamelikov@yandex.ru>
2017-09-24 10:14:15 +0300

    add wallet choose friend item

---
+++ WalletChooseFriendItem.m
@@ -0,0 +1,32 @@
+//
+//  WalletChooseFriendItem.m
+//  XenioUIKit
+//
+//  Created by Сергей Иванов on 24.09.17.
+//  Copyright © 2017 mifsoftware. All rights reserved.
+//
+
+#import "WalletChooseFriendItem.h"
+
+@interface WalletChooseFriendItem ()
+
+@property (strong) IBOutlet NSImageView *itemImageView;
+@property (strong) IBOutlet NSBox *itemSelectedIndicator;
+
+@end
+
+@implementation WalletChooseFriendItem
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do view setup here.
+}
+
+-(void)setRepresentedObject:(id)representedObject {
+    [super setRepresentedObject:representedObject];
+    if(representedObject != nil) {
+        [self.itemImageView setImage:[NSImage imageNamed:[representedObject valueForKey:@"itemImage"]]];
+    }
+}
+
+@end
