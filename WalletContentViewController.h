52d37579f62913fc1428273ffaaaf08f84573b0e
vanyamelikov <vanyamelikov@yandex.ru>
2017-09-25 05:58:09 +0300

    wallet 2.0

--- WalletContentViewController.h
+++ WalletContentViewController.h
@@ -7,7 +7,17 @@
 //
 
 #import <Cocoa/Cocoa.h>
+#import "WalletHistoryView.h"
+#import "SendMoneyButton.h"
 
 @interface WalletContentViewController : NSViewController
+
+@property (strong) IBOutlet WalletHistoryView *walletHistoryView;
+@property (strong) IBOutlet NSView *firstSendMoneyView;
+@property (strong) IBOutlet NSView *chooseFriendView;
+@property (strong) IBOutlet NSView *firstInput;
+@property (strong) IBOutlet NSBox *friendImageBox;
+@property (strong) IBOutlet NSImageView *friendImage;
+@property (strong) IBOutlet SendMoneyButton *sendMoneyButton;
 
 @end
--- WalletContentViewController.m
+++ WalletContentViewController.m
@@ -16,7 +16,45 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do view setup here.
+
+    self.walletHistoryView.hidden = YES;
+    self.firstSendMoneyView.hidden = NO;
+    self.chooseFriendView.hidden = YES;
+
+    NSClickGestureRecognizer *imageClick = [[NSClickGestureRecognizer alloc] initWithTarget:self action:@selector(imageAction:)];
+    [self.friendImage addGestureRecognizer:imageClick];
+
+    NSClickGestureRecognizer *sendMoneyClick = [[NSClickGestureRecognizer alloc] initWithTarget:self action:@selector(sendMoneyButtonClick:)];
+    [self.sendMoneyButton addGestureRecognizer:sendMoneyClick];
+}
+
+- (void)imageAction:(id)sender
+{
+    [NSAnimationContext runAnimationGroup:^(NSAnimationContext *context) {
+        context.duration = 0.5f;
+        self.firstSendMoneyView.animator.alphaValue = 0;
+    }
+                        completionHandler:^{
+                            self.firstSendMoneyView.hidden = YES;
+                            self.chooseFriendView.hidden = NO;
+                            self.firstSendMoneyView.alphaValue = 1;
+                        }];
+    
+}
+
+- (void)sendMoneyButtonClick:(id)sender
+{
+    [NSAnimationContext runAnimationGroup:^(NSAnimationContext *context) {
+        context.duration = 0.5f;
+        self.chooseFriendView.animator.alphaValue = 0;
+    }
+                        completionHandler:^{
+                            self.chooseFriendView.hidden = YES;
+                            self.firstSendMoneyView.hidden = NO;
+                            self.chooseFriendView.alphaValue = 1;
+                        }];
+    [self.friendImage setImage:[NSImage imageNamed:@"img_cf_a1"]];
+    
 }
 
 @end
