4c778e3697ad8c99969d6877a2945e5129c1e7b2
Иван Меликов <profit@MacBook-Pro-Ivan.local>
2017-10-05 05:28:08 +0300

    downloads done!

---
+++ DownloadsView.h
@@ -0,0 +1,13 @@
+//
+//  DownloadsView.h
+//  XenioUIKit
+//
+//  Created by Иван Меликов on 05.10.17.
+//  Copyright © 2017 mifsoftware. All rights reserved.
+//
+
+#import "NSView+INSNibLoading.h"
+
+@interface DownloadsView : INSNibLoadedView
+
+@end
---
+++ DownloadsView.m
@@ -0,0 +1,19 @@
+//
+//  DownloadsView.m
+//  XenioUIKit
+//
+//  Created by Иван Меликов on 05.10.17.
+//  Copyright © 2017 mifsoftware. All rights reserved.
+//
+
+#import "DownloadsView.h"
+
+@implementation DownloadsView
+
+- (void)drawRect:(NSRect)dirtyRect {
+    [super drawRect:dirtyRect];
+    
+    // Drawing code here.
+}
+
+@end
